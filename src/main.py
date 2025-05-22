628434bc76f072cc5f09e21f2aae5774b7facf01
GitSiege7 <carljklug@icloud.com>
2025-05-22 17:46:22 -0400

    added main, textnode

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+print("hello world")
+
+def main():
+    text_node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(text_node)
+
+main()
