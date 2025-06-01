335e557d7729c3d17e76479c8d9f95c6033b8388
GitSiege7 <carljklug@icloud.com>
2025-06-01 17:34:00 -0400

    added recursive copy

--- src/main.py
+++ src/main.py
@@ -1,6 +1,20 @@
-from parentnode import ParentNode
+import os
+import shutil
+
+def recursive_copy(source, destination):
+    shutil.rmtree(destination)
+    os.mkdir(destination)
+
+    for item in os.listdir(source):
+        if(os.path.isfile(source + "/" + item)):
+            shutil.copy(source + "/" + item, destination)
+        else:
+            os.mkdir(destination + "/" + item)
+            recursive_copy(source + "/" + item, destination + "/" + item)
+    return
+
 
 def main():
-    
+    recursive_copy("/home/siege/workspace/python/static_site_gen/static", "/home/siege/workspace/python/static_site_gen/public")
 
 main()
