95522b539f30fc0f58ad6cb6fc943b30614a48c0
GitSiege7 <carljklug@icloud.com>
2025-06-03 17:49:05 -0400

    updated for support for github pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -2,7 +2,7 @@ from markdown_to_htmlnode import markdown_to_htmlnode
 from extract_title import extract_title
 import os
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path) as fp:
@@ -17,24 +17,26 @@ def generate_page(from_path, template_path, dest_path):
 
     title = extract_title(md)
 
-    template_w_title = template.replace("{{ Title }}", title)
-    filled_template = template_w_title.replace("{{ Content }}", html)
+    template2 = template.replace("{{ Title }}", title)
+    template3 = template2.replace("{{ Content }}", html)
+    template4 = template3.replace("href=\"/", f"href=\"{basepath}")
+    template5 = template4.replace("src=\"/", f"src=\"{basepath}")
     
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     with open(dest_path, "w") as fp:
-        fp.write(filled_template)
+        fp.write(template5)
 
     return
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     
     if os.path.isfile(dir_path_content):
         new_dest = dest_dir_path.replace(".md", ".html")
         
-        generate_page(dir_path_content, template_path, new_dest)
+        generate_page(dir_path_content, template_path, new_dest, basepath)
     else:
         for dir in os.listdir(dir_path_content):
-            generate_pages_recursive(dir_path_content + "/" + dir, template_path, dest_dir_path+ "/" + dir)
+            generate_pages_recursive(dir_path_content + "/" + dir, template_path, dest_dir_path+ "/" + dir, basepath)
     
     return
--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import os
 import shutil
 from generate_page import generate_pages_recursive
+import sys
 
 def recursive_copy(source, destination):
-    shutil.rmtree(destination)
-    os.mkdir(destination)
+    shutil.rmtree(destination, True)
+    os.makedirs(destination, exist_ok=True)
 
     for item in os.listdir(source):
         if(os.path.isfile(source + "/" + item)):
@@ -16,11 +17,17 @@ def recursive_copy(source, destination):
 
 
 def main():
-    recursive_copy("/home/siege/workspace/python/static_site_gen/static", "/home/siege/workspace/python/static_site_gen/public")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    
+    recursive_copy("/home/siege/workspace/python/static_site_gen/static", "/home/siege/workspace/python/static_site_gen/docs")
 
     generate_pages_recursive("/home/siege/workspace/python/static_site_gen/content", 
                              "/home/siege/workspace/python/static_site_gen/template.html",
-                             "/home/siege/workspace/python/static_site_gen/public")
+                             "/home/siege/workspace/python/static_site_gen/docs",
+                             basepath)
 
 
 main()
